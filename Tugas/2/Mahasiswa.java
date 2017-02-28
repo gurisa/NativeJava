4fdcc44d17172f1eb3145fb84991102e97c2d448
gurisa <gurisadevs@gmail.com>
2017-02-28 18:51:24 +0700

    Tugas 2 - 10115253

---
+++ Tugas/2/Mahasiswa.java
@@ -0,0 +1,7 @@
+class Mahasiswa {
+  public static void main(String[] args) {
+    System.out.println("Nama   : Raka Suryaardi Widjaja");
+    System.out.println("NIM    : 10115253");
+    System.out.println("Alamat : Jl. Terusan Rancaekek Majalaya No. 289");
+  }
+}
